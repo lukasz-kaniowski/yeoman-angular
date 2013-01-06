0ec4ef46d557fb6723a9f3df158e2d806db5753e
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2013-01-06 22:09:25 +0000

    init angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var yeomanAngularApp = angular.module('yeomanAngularApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
